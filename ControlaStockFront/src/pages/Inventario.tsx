1f122c310f2f4b43d367f1bcb52971d9cb6de3f3
ErickMonteiroMDK <erickmonteiromdk@gmail.com>
2025-09-25 19:03:49 -0300

    Recriando novo front.

--- ControlaStockFront/src/pages/Inventario.tsx
+++
@@ -1,8 +0,0 @@
-export function InventarioPage() {
-  return (
-    <div>
-      <h1>Página de Inventário</h1>
-      <p>Esta página deve ser protegida!</p>
-    </div>
-  );
-}
--- ControlaStockFront/src/pages/Login.tsx
+++
@@ -1,7 +0,0 @@
-export function LoginPage() {
-  return (
-    <div>
-      <h1>Página de Login</h1>
-    </div>
-  );
-}
